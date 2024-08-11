0e53a7ed09674f04217a426335b7320f859c65a0
Eduardo Drolhe <edudrolhe@gmail.com>
2024-08-11 12:01:48 -0300

    feat: add BookingItem component

---
+++ src/app/constants/search.ts
@@ -0,0 +1,31 @@
+interface QuickSearchOption {
+  imageUrl: string
+  title: string
+}
+
+export const quickSearchOptions: QuickSearchOption = [
+  {
+    imageUrl: "Cabelo.svg",
+    title: "Cabelo",
+  },
+  {
+    imageUrl: "Barba.svg",
+    title: "Barba",
+  },
+  {
+    imageUrl: "Acabamento.svg",
+    title: "Acabamento",
+  },
+  {
+    imageUrl: "Messsage.svg",
+    title: "Massagem",
+  },
+  {
+    imageUrl: "Sobrancelha.svg",
+    title: "Sobrancelha",
+  },
+  {
+    imageUrl: "Hidratacao.svg",
+    title: "Hidratacao",
+  },
+]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,17 @@
 import { SearchIcon } from "lucide-react"
-import { Button } from "./_components/ui/button"
 import Header from "./_components/ui/header"
+import { Button } from "./_components/ui/button"
+
 import { Input } from "./_components/ui/input"
-import Image from "../../node_modules/next/image"
+import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
-import React from "react"
-import { Badge } from "./_components/ui/badge"
-import { Avatar, AvatarImage } from "./_components/ui/avatar"
 import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/barbershop-item"
-import { FootprintsIcon } from "lucide-react"
-import { EyeIcon } from "lucide-react"
+import { quickSearchOptions } from "./constants/search"
+import BookingItem from "./_components/ui/booking-item"
 
 const Home = async () => {
-  const barbershop = await db.barbershop.findMany({})
+  const barbershops = await db.barbershop.findMany({})
   const popularBarbershops = await db.barbershop.findMany({
     orderBy: {
       name: "desc",
@@ -26,7 +24,7 @@ const Home = async () => {
       <Header />
       <div className="p-5">
         {/* TEXTO */}
-        <h2 className="text-xl font-bold">Olá, Eduardo!</h2>
+        <h2 className="text-xl font-bold">Olá, Felipe!</h2>
         <p>Segunda-feira, 05 de agosto.</p>
 
         {/* BUSCA */}
@@ -39,37 +37,20 @@ const Home = async () => {
 
         {/* BUSCA RÁPIDA */}
         <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
-          <Button className="gap-2" variant="secondary">
-            <Image src="Cabelo.svg" width={16} height={16} alt="Cabelo" />
-            Cabelo
-          </Button>
-          <Button className="gap-2" variant="secondary">
-            <Image src="Barba.svg" width={16} height={16} alt="Barba" />
-            Barba
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <Image
-              src="Acabamento.svg"
-              width={16}
-              height={16}
-              alt="Acabamento"
-            />
-            Acabamento
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <FootprintsIcon size={16} />
-            Pézinho
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <EyeIcon size={16} />
-            Sobracelha
-          </Button>
+          {quickSearchOptions.map((option) => (
+            <Button className="gap-2" variant="secondary" key={option.title}>
+              <Image
+                src={option.imageUrl}
+                width={16}
+                height={16}
+                alt={option.title}
+              />
+              {option.title}
+            </Button>
+          ))}
         </div>
 
-        {/*IMAEGEM*/}
+        {/* IMAGEM */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             alt="Agende nos melhores com FSW Barber"
@@ -79,37 +60,14 @@ const Home = async () => {
           />
         </div>
 
-        {/*AGENDAMENTO*/}
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Agendamentos
-        </h2>
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            {/* ESQUERDA */}
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de Cabelo</h3>
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                </Avatar>
-                <p className="text-sm">Barbearia FSW</p>
-              </div>
-            </div>
-            {/*DIREITA*/}
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">05</p>
-              <p className="text-sm">20:00</p>
-            </div>
-          </CardContent>
-        </Card>
+        {/* AGENDAMENTO */}
+        <BookingItem />
 
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
         </h2>
         <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
-          {barbershop.map((barbershop) => (
+          {barbershops.map((barbershop) => (
             <BarbershopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
@@ -123,11 +81,12 @@ const Home = async () => {
           ))}
         </div>
       </div>
+
       <footer>
         <Card>
           <CardContent className="px-5 py-6">
-            <p className="test-gray-400 text-sm">
-              @ 2023 Copyright <span className="font-bold">FSW Barber</span>
+            <p className="text-sm text-gray-400">
+              © 2023 Copyright <span className="font-bold">FSW Barber</span>
             </p>
           </CardContent>
         </Card>
