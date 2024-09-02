7cd4c780f0ee8b3ab7b493330db4db7162172405
Eduardo Drolhe <edudrolhe@gmail.com>
2024-09-02 13:23:32 -0300

    feat: ensure only authenticated user can hook

--- src/app/_actions/create-booking.ts
+++ src/app/_actions/create-booking.ts
@@ -1,7 +1,9 @@
 "use server"
 
 import { revalidatePath } from "../../../node_modules/next/cache"
+import { authOptions } from "../_lib/auth"
 import { db } from "../_lib/prisma"
+import { getServerSession } from "next-auth"
 
 interface CreateBookingParams {
   userId: string
@@ -10,6 +12,10 @@ interface CreateBookingParams {
 }
 
 export const createBooking = async (params: CreateBookingParams) => {
+  const user = await getServerSession(authOptions)
+  if (!user) {
+    throw new Error("Usuário não autenticado")
+  }
   await db.booking.create({
     data: params,
   })
