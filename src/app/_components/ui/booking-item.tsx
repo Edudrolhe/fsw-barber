398eb40e7ff4f698862225595976ad27bcf8fcc7
Eduardo Drolhe <edudrolhe@gmail.com>
2024-09-29 12:20:26 -0300

    feat: ensure booking item takes full width on home page

--- src/app/_components/ui/booking-item.tsx
+++ src/app/_components/ui/booking-item.tsx
@@ -68,8 +68,8 @@ const BookingItem = ({ booking }: BookingItemProps) => {
 
   return (
     <Sheet open={isSheetOpen} onOpenChange={handleSheetOpenChange}>
-      <SheetTrigger className="w-full">
-        <Card className="w-full min-w-[90%]">
+      <SheetTrigger className="w-full min-w-[90%]">
+        <Card className="min-w-[90%]">
           <CardContent className="flex justify-between p-0">
             {/* ESQUERDA */}
             <div className="flex flex-col gap-2 py-5 pl-5">
